b353c6edcf9a99de9f814c7f36ab604025664175
livebits <ahmad.khalilfar@gmail.com>
2024-02-11 13:43:00 +0330

    add test coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/', '\\.d\\.ts$'],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
